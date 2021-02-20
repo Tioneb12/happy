ae260ec11551761bc338d11115768c72623a07af
Ben <benoit.zeller@gmail.com>
2021-02-20 21:26:02 +0100

    feat(organisation): create model

---
+++ db/schema.rb
@@ -0,0 +1,42 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_02_20_195627) do
+
+  create_table "associations", force: :cascade do |t|
+    t.string "association_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "organisationable_type", null: false
+    t.integer "organisationable_id", null: false
+    t.index ["organisationable_type", "organisationable_id"], name: "index_associations_on_organisationable"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "organisationable_type", null: false
+    t.integer "organisationable_id", null: false
+    t.index ["organisationable_type", "organisationable_id"], name: "index_companies_on_organisationable"
+  end
+
+  create_table "organisations", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "address2"
+    t.integer "zip_code"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
